f5ffbe65cbb316a954bd80dd292bb69541221a5f
utarwyn <maxime.malgorn@laposte.net>
2018-01-13 16:55:39 +0100

    Mon premier commit

---
+++ src/main/java/fr/utaria/exemple/Exemple.java
@@ -0,0 +1,15 @@
+package fr.utaria.exemple;
+
+import fr.utaria.utariacore.UtariaPlugin;
+
+public class Exemple extends UtariaPlugin {
+
+	public void onEnable() {
+
+	}
+
+	public void onDisable() {
+
+	}
+
+}
